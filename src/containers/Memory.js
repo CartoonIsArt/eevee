398876fffdeae8009198d4040230e17bf2433bc7
kswgit <hero.sungwoo@gmail.com>
2017-05-27 22:48:47 +0900

    Update s

--- src/containers/Memory.js
+++ src/containers/Memory.js
@@ -6,7 +6,7 @@ class Memory extends Component {
   render() {
     return (
       <article style={{height: "1036px"}}>
-        <div style={{height: "640px"}} > doc </div>
+        <div style={{height: "640px"}} > docs </div>
         <div style={{height: "396px"}} > com </div>
       </article>
     )
