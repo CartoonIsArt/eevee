d630de61dd7632510cb90a01b1306761b844ad8d
cocoapi <eksrns22tp@gmail.com>
2017-05-27 23:38:58 -0700

    Make Nav component

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -7,19 +7,20 @@ class Nav extends Component {
     return (
       <Affix>
         <nav className="Nav-wrapper">
-          <div className="Nav" style={{display:'flex', flexDirection:'row', alignContent:"left"}}>
-            <div style={{width:"10%"}}>
+          <div className="Nav" style={{ display: 'flex', flexDirection: 'row', alignContent: 'left' }}>
+            <div style={{ width: '10%' }}>
               <a className="ant-anchor-link-title" href="#" width="10%">
                 <img height="44px" width="107px" src="https://cia.kw.ac.kr/media/logo.png" alt="logo" />
               </a>
             </div>
-            <div style={{display:"flex", width:"80%", padding: "7px"}}>
-              <Search placeholder="검색(ex: 16기 김성우)"
-                      style={{width: "50%"}}
-                      onsearch={value => console.log(value)}
-               />
+            <div style={{ display: 'flex', width: '80%', padding: '7px' }}>
+              <Search
+                placeholder="검색(ex: 16기 김성우)"
+                style={{ width: '50%' }}
+                onsearch={value => console.log(value)}
+              />
             </div>
-            <div style={{width:"10%"}}>
+            <div style={{ width: '10%' }}>
               toggle
             </div>
           </div>
