12b08d0aa82cfaa6fbc3d83dd13435525ac15493
Songnari <na061300@naver.com>
2017-05-27 23:41:04 -0700

    Make Noties title

--- src/containers/Noties.js
+++ src/containers/Noties.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Affix, Row, Col } from 'antd'
+import { Icon, Badge } from 'antd'
 
 
 class Noties extends Component {
@@ -7,7 +7,17 @@ class Noties extends Component {
     const iter = [1, 2, 3]
     return (
       <div style={{ height: '192px' }}>
-        <div style={{ height: '25%' }}> noties                                                    </div>
+        <div style={{ height: '25%', padding: '12px', fontSize: '12pt', textAlign: 'left', display: 'flex', flexDrection: 'column' }}>
+          <div style={{ marginRight: '12px', fontSize: '18pt' }}>
+            <Icon type="notification" />
+            알람
+          </div>
+          <div style={{ fontSize: 'small', marginTop: '4px' }}>
+            <Badge dot>
+              <a href="#">Link someting</a>
+            </Badge>
+          </div>
+        </div>
         {iter.map(i =>
           (<div key={i} style={{ height: '25%', display: 'flex', alignItems: 'stretch' }}>
             <div style={{ width: '48px', height: '48px' }}>
