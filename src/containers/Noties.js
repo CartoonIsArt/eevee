3f2f03a922136e6aa180a307364e70e5e86a457b
Songnari <na061300@naver.com>
2017-05-28 00:13:19 -0700

    Make Noties image

--- src/containers/Noties.js
+++ src/containers/Noties.js
@@ -20,8 +20,8 @@ class Noties extends Component {
         </div>
         {iter.map(i =>
           (<div key={i} style={{ height: '25%', display: 'flex', alignItems: 'stretch' }}>
-            <div style={{ width: '48px', height: '48px' }}>
-              pic
+            <div style={{ width: '48px', height: '48px', borderRadius: '24px', overflow: 'hidden' }}>
+              <img width="100%" height="100%" src="https://cia.kw.ac.kr/media/04531d51-2429-4a8a-9ec5-32d5f00a544b.PNG" alt="profile" />
             </div>
             <div style={{ flexGrow: '2', display: 'flex', alignItems: 'stretch', flexDirection: 'column' }}>
               <div > time </div>
