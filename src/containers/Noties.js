3d20ef647cbd40bb61a080e5e6febb76f4527601
Songnari <na061300@naver.com>
2017-06-10 00:12:26 -0700

    add popconfirm at name

--- src/containers/Noties.js
+++ src/containers/Noties.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react'
-import { Icon, Badge } from 'antd'
-
+import { Icon, Popconfirm } from 'antd'
+import Line from '../components/Line'
 
 class Noties extends Component {
   render() {
     const iter = [1, 2, 3]
     return (
-      <div style={{ height: '192px' }}>
-        <div style={{ height: '25%', padding: '12px', fontSize: '12pt', textAlign: 'left', display: 'flex', flexDrection: 'column' }}>
-          <div style={{ marginRight: '12px', fontSize: '18pt' }}>
+      <div style={{ height: '192px', backgroundColor: 'white', padding: '4px' }}>
+        <div style={{ height: '15%', fontSize: '12pt', textAlign: 'left', display: 'flex', flexDrection: 'column' }}>
+          <div style={{ marginRight: '240px', marginLeft: '12px', fontSize: '12pt' }}>
             <Icon type="notification" />
-            알람
           </div>
-          <div style={{ fontSize: 'small', marginTop: '4px' }}>
-            <Badge dot>
-              <a href="#">Link someting</a>
-            </Badge>
+          <div style={{ fontSize: '12pt', marginTop: '4px' }}>
+            <a href="#">설정</a>
           </div>
         </div>
+        <Line />
         {iter.map(i =>
-          (<div key={i} style={{ height: '25%', display: 'flex', alignItems: 'stretch' }}>
-            <div style={{ width: '48px', height: '48px' }}>
-              pic
+          (<div>
+            <div key={i} style={{ height: '25%', display: 'flex', alignItems: 'stretch' }}>
+              <div style={{ width: '48px', height: '48px', borderRadius: '24px', overflow: 'hidden' }}>
+                <a href="#"><img width="100%" src="https://cia.kw.ac.kr/media/1646b774-0365-4001-8285-42033807b32d.jpg" alt="profile" /></a>
+              </div>
+              <div style={{ flexGrow: '2', display: 'flex', alignItems: 'stretch', flexDirection: 'column' }}>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ marginRight: '24px', marginLeft: '12px' }}>
+                    <Popconfirm title="Are you sure delete this task?">
+                      <a href="#"> 임원진</a>
+                    </Popconfirm>
+                  </div>
+                  <div style={{ color: '#dfdfdf' }}> 2017-06-10 </div>
+                </div>
+                <div style={{ flexGrow: '1', display: 'flex', marginLeft: '12px' }}> 공지알람글 </div>
+              </div>
             </div>
-            <div style={{ flexGrow: '2', display: 'flex', alignItems: 'stretch', flexDirection: 'column' }}>
-              <div > time </div>
-              <div style={{ flexGrow: '1' }}> asdf </div>
-            </div>
+            <Line />
           </div>),
         )}
       </div>
