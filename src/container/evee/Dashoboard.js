2399ace27c4ad800826496b0a18906950431edd1
sGOM <pooh6195@naver.com>
2017-07-10 20:38:45 +0900

    활동인구 등록2 사용자가 더 버튼을 누르고 싶도록 사진 오른쪽의 이점설명란 아래에버튼을 배치했습니다. 위와 같은 이유로 좀 더 자세한 활동인구에 대한 설명은 가장하단에 배치했습니다.

--- src/container/evee/Dashoboard.js
+++ src/container/evee/Dashoboard.js
@@ -4,33 +4,33 @@ import { Button } from 'antd'
 class Dashboard extends Component {
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', padding: '20px', width: '1000px', height: '1618px' }}>
-
-        <div style={{ height: '48px', width: '100%', fontSize: '22pt', fontWeight: 'bold', color: '#FF8900', backgroundColor: 'white', borderRadius: '0.25em' }}>
-    &nbsp;||&nbsp; 활동인구 등록
-    </div>
-
-        <div style={{ height: '20px' }} />
-        <div style={{ overflow: 'hidden', height: '180px', width: '100%' }}>
-          <img width="100%" src="http://i.imgur.com/Erf8prO.jpg" alt="활동인구" />
+      <div style={{ display: 'flex', flexDirection: 'column', backgroundColor: 'white', margin: '0px 8px 0px 8px', width: '1040px', padding: '0px 20px 0px 20px' }}>
+        <div style={{ height: '10px' }} />
+        <div style={{ width: '100%', fontSize: '32px', fontWeight: 'bold', color: '#1976D2' }}>
+        ● 지금 활동인구에 등록해 보세요!
         </div>
         <div style={{ height: '20px' }} />
-
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', border: '2px solid #1976D2', borderRadius: '0.25em', backgroundColor: 'white' }}>
-          <div style={{ width: '100%', textAlign: 'center', padding: '20px', fontSize: '25pt', borderBottom: '1px solid black' }}>
-      활동인구란?
-      </div>
-          <div style={{ padding: '15px', fontSize: '17pt' }}>
-      활동인구 설명<br /><br /><br /><br /><br /><br /><br />설명 끝
-      </div>
-          <div style={{ width: '100%', padding: '20px', borderTop: '1px solid black', textAlign: 'center' }} >
-            <Button style={{ fontSize: '10pt', fontWeight: 'bold' }}> 활동인구<br /> 등록하기!!</Button>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', overflow: 'hidden' }}>
+          <div style={{ height: '388px', width: '69%' }}>
+            <img src="http://i.imgur.com/xOR7aaQ.jpg" alt="활동인구" />
+          </div>
+          <div style={{ width: '20px' }} />
+          <div style={{ display: 'flex', flexDirection: 'column', width: '29%', height: '388px' }}>
+            <div style={{ height: '100%', padding: '8px', fontSize: '16px', fontWeight: 'bold', border: '1px solid black', borderRadius: '4px' }}>
+            ● 동아리 회의 투표권<br />● 동방 재실권<br />● 동게 프리미엄 계정
+            </div>
+            <div style={{ height: '20px' }} />
+            <div style={{ textAlign: 'center' }}>
+              <Button style={{ fontSize: '12pt', fontWeight: 'bold', backgroundColor: '#1976D2', color: 'white' }}>≫ 활동인구 등록하기!!</Button>
+            </div>
           </div>
         </div>
-
+        <div style={{ height: '20px' }} />
+        <div style={{ fontSize: '16px', fontWeight: 'bold', padding: '16px', border: '2px solid #1976D2', borderRadius: '4px' }}>
+        활동인구는 ~~~~
+        </div>
       </div>
     )
   }
 }
-
 export default Dashboard
