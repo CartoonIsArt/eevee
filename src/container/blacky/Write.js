a5397b2bd5976317ec76c6bdd880639431f7be98
kswgit <hero.sungwoo@gmail.com>
2017-07-13 18:08:05 +0900

    글 쓸 때 미리보기 기능 추가
    기존동게 미리보기 버튼이 누르기 불편한 곳에 있어서 잘 누르지 않음.
    글 업로드를 하면서 자연스럽게 미리보기 할 수 있도록 수정.

--- src/container/blacky/Write.js
+++ src/container/blacky/Write.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 import { Button, Input } from 'antd'
+import ReactMarkdown from 'react-markdown'
 
 class Write extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      text: '',
+      mode: 'edit',
+    }
+  }
   render() {
+    const text = this.state.text
     const user = this.props.user
+    const mode = this.state.mode
     return (
       <div style={{ marginBottom: '4px', padding: '4px', display: 'flex', background: '#FFF' }} >
         <div style={{ marginRight: '4px', width: '48px', height: '48px', background: '#FFF', overflow: 'hidden' }} >
@@ -12,12 +22,38 @@ class Write extends Component {
         }
         </div>
         <div style={{ flexGrow: 1 }}>
-          <Input type="textarea" autosize={{ minRows: 3, maxRows: 6 }} style={{ width: '100%' }} />
+          {mode === 'edit' ?
+            <Input 
+              type="textarea" 
+              autosize={{ minRows: 3, maxRows: 6 }} 
+              style={{ width: '100%' }}
+              value={text}
+              onChange={e => this.setState({text: e.target.value})}
+            /> :
+            mode === 'preview' ?
+            <ReactMarkdown source={text} /> :
+            <div />
+          }
           <div style={{ justifyContent: 'space-between', display: 'flex', margin: '4px 0px' }} >
             <Button icon="picture" shape="circle" />
-            <Button>
-              붸에에에에에에
-            </Button>
+            <div>
+            {mode === 'edit' ?
+              <Button icon='edit' onClick={() => this.setState({mode: 'preview'})}>
+                글쓸거임?
+              </Button> :
+              mode === 'preview' ?
+              <div style={{display: 'flex' }}>
+                <Button icon="reload" onClick={() => this.setState({mode: 'edit'})}>
+                  수정
+                </Button> 
+                <div style={{width: '4px'}} />
+                <Button icon="cloud-upload" type="primary" onClick={() => console.log(text)}>
+                  완료
+                </Button>
+              </div> :
+              <div />
+            }
+            </div>
           </div>
         </div>
       </div>
