34ad5a62fd8470a91c318ccb69857aba08c7aa4e
sGOM <pooh6195@naver.com>
2017-07-23 15:20:42 +0900

    활동인구 등록3 사용자원을 줄이기위해 div로 주던 간격을 margin으로 변경 촌스런 'ㅁ'한자를 멋스런 아이콘으로 변경 등록시 이점이 더 잘보이도록 글씨크기 확대

--- src/container/evee/Dashoboard.js
+++ src/container/evee/Dashoboard.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Button, Icon } from 'antd'
 
 class Dashboard extends Component {
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', backgroundColor: 'white', margin: '0px 8px 0px 8px', width: '1040px', padding: '0px 20px 0px 20px' }}>
-        <div style={{ height: '10px' }} />
-        <div style={{ width: '100%', fontSize: '32px', fontWeight: 'bold', color: '#1976D2' }}>
-        ● 지금 활동인구에 등록해 보세요!
+      <div style={{ display: 'flex', flexDirection: 'column', backgroundColor: 'white', margin: '0px 8px', width: '1040px', padding: '0px 20px' }}>
+        <div style={{ width: '100%', fontSize: '32px', fontWeight: 'bold', color: '#1976D2', margin: '20px 0px 10px 0px' }}>
+          <Icon type="right-square-o" /> 지금 활동인구에 등록해 보세요!
         </div>
-        <div style={{ height: '20px' }} />
-        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', overflow: 'hidden' }}>
-          <div style={{ height: '388px', width: '69%' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', margin: '10px 0px' }}>
+          <div style={{ height: '388px', width: '69%', marginRight: '20px', overflow: 'hidden' }}>
             <img src="http://i.imgur.com/xOR7aaQ.jpg" alt="활동인구" />
           </div>
-          <div style={{ width: '20px' }} />
-          <div style={{ display: 'flex', flexDirection: 'column', width: '29%', height: '388px' }}>
-            <div style={{ height: '100%', padding: '8px', fontSize: '16px', fontWeight: 'bold', border: '1px solid black', borderRadius: '4px' }}>
-            ● 동아리 회의 투표권<br />● 동방 재실권<br />● 동게 프리미엄 계정
+          <div style={{ justifyContent: 'space-between', display: 'flex', flexDirection: 'column', padding: '8px', width: '29%', border: '1px solid black', borderRadius: '4px', height: '388px' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', height: '100%', fontSize: '20px', fontWeight: 'bold' }}>
+              <div><Icon type="right-circle" />동아리 회의 투표권</div>
+              <div><Icon type="right-circle" />동방 재실권</div>
+              <div><Icon type="right-circle" />동게 프리미엄 계정</div>
             </div>
-            <div style={{ height: '20px' }} />
             <div style={{ textAlign: 'center' }}>
-              <Button style={{ fontSize: '12pt', fontWeight: 'bold', backgroundColor: '#1976D2', color: 'white' }}>≫ 활동인구 등록하기!!</Button>
+              <Button size="large" icon="double-right" style={{ fontSize: '14pt', fontWeight: 'bold', backgroundColor: '#1976D2', color: 'white' }}> 활동인구 등록하기!!</Button>
             </div>
           </div>
         </div>
-        <div style={{ height: '20px' }} />
-        <div style={{ fontSize: '16px', fontWeight: 'bold', padding: '16px', border: '2px solid #1976D2', borderRadius: '4px' }}>
+        <div style={{ fontSize: '18px', fontWeight: 'bold', padding: '16px', border: '2px solid #1976D2', borderRadius: '4px', margin: '10px 0px' }}>
         활동인구는 ~~~~
         </div>
       </div>
