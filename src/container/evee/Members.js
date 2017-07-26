57198507722b8b159d347f9292d95e58793c1964
Songnari <na061300@naver.com>
2017-07-26 04:27:54 -0700

    프로필 변경 틀 제작 #38
    한 페이지에 얼마나 창이 차지하는지 알기위해 틀만 만들었습니다.
    생일은 보다 편하게 적기 위해 달력으로 입력하게 했습니다.
    핸드폰 번호도 첫 시작은 주로 010 이기때문에 앞에 010으로 고정했습니다.

--- src/container/evee/Members.js
+++ src/container/evee/Members.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Form, DatePicker, Input } from 'antd';
 
-class Members extends Component {
+const FormItem = Form.Item;
+
+class RegistrationForm extends React.Component {
   render() {
     return (
-      <div>
-        <h1> Members </h1>
+      <div style={{ width: '100%', background: '#ffffff', marginLeft: '8px', marginTop: '8px' }}>
+        <div style={{ width: '400px', marginTop: '100px', marginLeft: '80px' }}>
+          <Form onSubmit={this.handleSubmit}>
+            <FormItem
+              label="Nickname"
+            >
+              <Input placeholder="ex) 19기 xxx" />
+            </FormItem>
+            <FormItem
+              label="E-mail"
+            >
+              <Input />
+            </FormItem>
+            <FormItem
+              label="DatePicker"
+            >
+              <DatePicker />
+            </FormItem>
+            <FormItem
+              label="학과"
+            >
+              <Input placeholder="ex) 컴퓨터소프트웨어학과" />
+            </FormItem>
+            <FormItem
+              label="Phone Number"
+            >
+              <Input addonBefore={'010'} style={{ width: '100%' }} placeholder="ex) 1234 - 1234" />
+            </FormItem>
+            <FormItem
+              label="좋아하는 캐릭터"
+            >
+              <Input placeholder="ex) 하이큐 히나타" />
+            </FormItem>
+          </Form>
+        </div>
       </div>
-    )
+    );
   }
 }
+
+const Members = Form.create()(RegistrationForm);
 
 export default Members
