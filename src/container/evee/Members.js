67a768c639505c4589f8817f365a4eb0804c8d96
cocoapi <eksrns22tp@gmail.com>
2017-07-29 00:17:45 -0700

    회원보기 생성 및 등급변경기능 추가 #39
    회원보기 컴포넌트 생성
    임시값으로 멤버한명만을 렌더링
    등급변경을 함수를통해 구현 및 콘솔로그를 통해 값 확인가능.
    로그값과 컴포넌트 외곽선은 추후 제거예정.

--- src/container/evee/Members.js
+++ src/container/evee/Members.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react'
+import { Select } from 'antd'
+
+const Option = Select.Option
+
+const user = {
+  name: '13기 송민준',
+  grade: '정회원',
+}
+
+function handleChange(value) {
+  user.grade = value;
+  console.log(user.grade);
+}
 
 class Members extends Component {
   render() {
     return (
-      <div>
-        <h1> Members </h1>
+      <div style={{ display: 'flex', flexDirection: 'column', width: '1000px', margin: '0px 8px', padding: '0px 20px', background: '#FFFFFF' }}>
+        <div style={{ height: '2%' }} />
+        <div style={{ height: '5%' }}>
+          <h1> 회원목록 </h1>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', padding: '8px', width: '944px', height: '80%', outline: 'solid black 1px' }}>
+          <div style={{ height: '5%', display: 'flex', flexDirection: 'row' }}>
+            <div style={{ width: '25%' }}>
+              <a> 기수 이름 </a>
+            </div>
+            <div style={{ width: '15%' }}>
+              <a> 활동여부</a>
+            </div>
+            <div style={{ width: '15%' }}>
+              <a> 회의참석</a>
+            </div>
+            <div style={{ width: '15%' }}>
+              <a> 회비납부</a>
+            </div>
+            <div style={{ width: '15%' }}>
+              <a> 회원등급</a>
+            </div>
+            <div style={{ width: '15%' }}>
+              <a> 징계</a>
+            </div>
+          </div>
+          <div style={{ height: '5%', display: 'flex', flexDirection: 'row' }}>
+            <div style={{ width: '25%' }}>
+              <a> {user.name} </a>
+            </div>
+            <div style={{ width: '15%' }} />
+            <div style={{ width: '15%' }} />
+            <div style={{ width: '15%' }} />
+            <div style={{ width: '15%' }}>
+              <Select
+                size="small"
+                defaultValue={user.grade}
+                style={{ width: 80 }}
+                onChange={handleChange}
+              >
+                <Option value="준회원"> 준회원 </Option>
+                <Option value="정회원"> 정회원 </Option>
+                <Option value="임원"> 임원</Option>
+              </Select>
+            </div>
+            <div style={{ width: '15%' }} />
+          </div>
+        </div>
       </div>
     )
   }
