10b34588d15b7c3de4dc7ac829d9e0891deeede9
sGOM <pooh6195@naver.com>
2017-09-23 14:10:17 +0900

    마진 수정
    마진의 px 을 4의 배수로 수정

--- src/container/evee/Dashboard.js
+++ src/container/evee/Dashboard.js
@@ -29,9 +29,9 @@ class Dashboard extends Component {
               </div>
               <div style={{ justifyContent: 'space-between', display: 'flex', flexDirection: 'column', padding: '8px', width: '29%', border: '1px solid black', borderRadius: '4px', height: '388px' }}>
                 <div style={{ display: 'flex', flexDirection: 'column', height: '100%', fontSize: '20px', fontWeight: 'bold' }}>
-                  <div style={{ margin: '6px 0px' }}><Icon size="small" style={{ margin: '4px' }} type="inbox" />동아리 회의 투표권</div>
-                  <div style={{ margin: '6px 0px' }}><Icon size="small" style={{ margin: '4px' }} type="home" />동방 재실권</div>
-                  <div style={{ margin: '6px 0px' }}><Icon size="small" style={{ margin: '4px' }} type="user-add" />동게 프리미엄 계정</div>
+                  <div style={{ margin: '8px 0px' }}><Icon size="small" style={{ margin: '4px' }} type="inbox" />동아리 회의 투표권</div>
+                  <div style={{ margin: '8px 0px' }}><Icon size="small" style={{ margin: '4px' }} type="home" />동방 재실권</div>
+                  <div style={{ margin: '8px 0px' }}><Icon size="small" style={{ margin: '4px' }} type="user-add" />동게 프리미엄 계정</div>
                 </div>
                 <div style={{ textAlign: 'center' }}>
                   <a href="https://cia.kw.ac.kr/wiki/index.php?title=%ED%9A%8C%EC%B9%99#.EC.A0.9C3.EC.A0.88_.EC.A0.95.ED.9A.8C.EC.9B.90">
