7ffdfda863a5ae6e4cb8d5d6a9aa439c01fb8208
sGOM <pooh6195@naver.com>
2017-10-02 02:34:35 +0900

    유저페이지 만들기 #64
    유저페이지 기본틀 작성 - 친숙하고 멋진
     트위터, 페이스북과 비슷하게 구성함
    중간 저장

--- src/container/evee/Userpage.js
+++ src/container/evee/Userpage.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { Button } from 'antd'
 
 class Userpage extends Component {
   render() {
+      const myMenu = {
+          color: '#365899', float: 'left', height:'44px', padding:'0px 16px', fontSize: '16px', textAlign: 'center', borderRight: '1px solid #e9eaed'
+      }
     return (
-      <div style={{ display: 'flex' }}>
-      USERPAGE
-      {this.props.match.params.username}
+      <div style={{
+        height: '1040px',
+        width: '1040px',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
+      }}>
+        <div style={{ height: '252px', border: '1px solid #e9eaed', margin: '12px' }}>
+          <div style={{ height: '208px', backgroundImage: "url('https://s20.postimg.org/m5mwyzknh/53e2909f.jpg')", backgroundPosition: 'center 30%' }}>
+            <div style={{
+                border: 'solid 4px white',
+                width: '208px',
+                height: '208px',
+                borderRadius: '104px',
+                position: 'relative',
+                left: '44px',
+                top: '48px',
+                zIndex: '2',
+                overflow: 'hidden',
+            }}>
+              <img src='https://i.imgur.com/tAxNVWy.jpg' style={{ maxWidth: '100%', height: 'auto' }}/>
+            </div>
+          </div>
+          <div style={{ position: 'relative', height: '44px', border: 'solid 1px #e9eaed', paddingLeft:'280px',}}>
+            <ul style={{ width: '100%', lineHeight: '2.4', fontSize: '12pt', }}>
+              <li><a href="" style={ myMenu }>내가쓴글</a></li>
+              <li><a href="" style={ myMenu }>좋아요 누른글</a></li>
+              <li><a href="" style={ myMenu }>댓글단 글</a></li>
+              <li><a href="/members" style={ myMenu }>회원들</a></li>
+              <Button type="dashed" style={{ inline: '2.4' ,fontSize: '12pt', marginLeft: '220px' }}>
+                프로필 수정
+              </Button>
+            </ul>
+          </div>
+        </div>
+        <div style={{ display: 'flex', border: 'solid 1px #e9eaed', margin: '12px', }}>
+          <div style={{ display: 'flex', width: '20%', padding: '16px' , border: 'solid 1px #e9eaed', margin: '12px 0px 12px 12px', }}>
+            <div style={{ width: '30%', textAlign: 'center', marginRight: '24px' }}>
+              기수  <br/>
+              이름  <br/>
+              학번  <br/>
+              학과  <br/>
+              활동인구  <br/>
+              정회원  <br/>
+            </div>
+            <div style={{ width: '70%', }}>
+              {this.props.match.params.last_name}  <br/>
+              {this.props.match.params.username}  <br/>
+              {this.props.match.params.student_number}  <br/>
+              {this.props.match.params.department}  <br/>
+              {this.props.match.params.isActivated}  <br/>
+              {this.props.match.params.isRegularMember}  <br/>
+            </div>
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', width:'80%', border: 'solid 1px #e9eaed', margin: '12px' }}>
+            <div style={{ padding: '12px', borderBottom: 'solid 1px #e9eaed' }}>
+              내가 쓴글
+            </div>
+            <div>
+              글글
+            </div>
+          </div>
+        </div>
+        {this.props.match.params.username}
       </div>
     )
   }
