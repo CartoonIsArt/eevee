7c244ac1b5bad425457967c39bf89ccb6f9c728d
sGOM <pooh6195@naver.com>
2017-10-05 00:55:57 +0900

    유저 페이지 만들기 #64
    예쁘게 아이콘 추가,
    메뉴에 마우스 올리면 회색으로 변해 페이지에 흥미?를 같도록 함,
    왼쪽 내 정보가 항상 보이도록(허전하지 않게),
    <br/>을 <ul><li>태그로 변경하여 더 깔쌈한 코드로,

--- src/container/evee/Userpage.js
+++ src/container/evee/Userpage.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { Button } from 'antd'
+import { Button, Icon } from 'antd'
 import { getMembers } from '../../actions'
 
 class Userpage extends Component {
   componentWillMount() {
     this.props.getMembers()
   }
+  check(boolean) {
+      if(boolean){
+          <Icon type="check" />
+      }
+      else{
+          <Icon type="close" />
+      }
+  }
   render() {
     const members = this.props.members
     const username = this.props.match.params.username
     const member = members.length > 0 ? members.filter(m => m.username === username) : []
     const myMenu = {
-      color: '#365899', float: 'left', height: '44px', padding: '0px 16px', fontSize: '16px', textAlign: 'center', borderRight: '1px solid #e9eaed',
+      color: '#365899', float: 'left', height: '44px', padding: '0px 16px', fontSize: '12pt', fontWeight: 'bold', textAlign: 'center', borderRight: '1px solid #e9eaed',
     }
     return (
       <div style={{
@@ -21,69 +29,100 @@ class Userpage extends Component {
         width: '100%',
         display: 'flex',
         flexDirection: 'column',
-        backgroundColor: 'white',
       }}
       >
-        <div style={{ height: '252px', border: '1px solid #e9eaed', margin: '12px' }}>
-          <div style={{ height: '208px', backgroundImage: "url('https://s20.postimg.org/m5mwyzknh/53e2909f.jpg')", backgroundPosition: 'center 30%' }}>
+        <div style={{ height: '280px', border: '1px solid #d3d6db', margin: '12px', backgroundColor:'#fff' }}>
+          <div style={{ height: '236px', backgroundImage: "url('https://i.imgur.com/RcuU2lm.png')", backgroundPosition: 'center 30%' }}>
             <div style={{
               border: 'solid 4px white',
               width: '208px',
               height: '208px',
-              borderRadius: '104px',
+              borderRadius: '50%',
               position: 'relative',
               left: '44px',
-              top: '48px',
+              top: '76px',
               zIndex: '2',
               overflow: 'hidden',
             }}
             >
-              <img src="https://i.imgur.com/tAxNVWy.jpg" style={{ maxWidth: '100%', height: 'auto' }} alt="bg-img" />
+              <img src="https://i.imgur.com/tAxNVWy.jpg" style={{ maxWidth: '100%', height: 'auto' }} alt="Profile-img" />
             </div>
           </div>
-          <div style={{ position: 'relative', height: '44px', border: 'solid 1px #e9eaed', paddingLeft: '280px' }}>
+          <div style={{ position: 'relative', height: '44px', paddingLeft: '340px' }}>
             <ul style={{ width: '100%', lineHeight: '2.4', fontSize: '12pt' }}>
-              <li><a href="" style={myMenu}>내가쓴글</a></li>
-              <li><a href="" style={myMenu}>좋아요 누른글</a></li>
-              <li><a href="" style={myMenu}>댓글단 글</a></li>
-              <li><a href="/members" style={myMenu}>회원들</a></li>
-              <Button type="dashed" style={{ inline: '2.4', fontSize: '12pt', marginLeft: '220px' }}>
-                프로필 수정
+              <li><a className="menu" href="" style={myMenu}>내가쓴글</a></li>
+              <li><a className="menu" href="" style={myMenu}>좋아요 누른글</a></li>
+              <li><a className="menu" href="" style={myMenu}>댓글단 글</a></li>
+              <li><a className="menu" href="/members" style={myMenu}>회원들</a></li>
+              <Button type="dashed" style={{ inline: '2.4', fontSize: '12pt', marginLeft: '372px' }}>
+                <Icon type="tool" /> 프로필 수정
               </Button>
             </ul>
           </div>
         </div>
-        <div style={{ display: 'flex', border: 'solid 1px #e9eaed', margin: '12px' }}>
-          <div style={{ display: 'flex', width: '20%', padding: '16px', border: 'solid 1px #e9eaed', margin: '12px 0px 12px 12px' }}>
-            <div style={{ width: '30%', textAlign: 'center', marginRight: '24px' }}>
-              기수<br />
-              이름<br />
-              학번<br />
-              학과<br />
-              활동인구<br />
-              정회원<br />
-            </div>
-            {member.length > 0 &&
-              <div style={{ width: '70%' }}>
-                {member[0].last_name}<br />
-                {member[0].last_name}<br />
-                {member[0].student_number}<br />
-                {member[0].department}<br />
-                {member[0].isActivated}<br />
-                {member[0].isRegularMember}
+        <div style={{ display: 'flex', margin: '12px', fontSize: '10pt' }}>
+          <div style={{ display: 'block', width: '20%'}}>
+            <div style={{ position: 'sticky', top: '52px', display: 'flex', flexDirection: 'column', backgroundColor:'#fff', border: 'solid 1px #d3d6db', }}>
+              <div style={{ fontSize: '12pt' ,backgroundColor: '#f6f7f9', padding: '12px', fontWeight: 'bold', borderBottom: 'solid 1px #d3d6db', }}><Icon type="user" /> 내 정보</div>
+              <div style={{ display: 'flex', padding: '8px'  }}>
+                <div style={{ width: '36%', textAlign: 'center', marginRight: '24px' }}>
+                  <ol>
+                    <li style={{ margin: '8px 0px' }}>기수</li>
+                    <li style={{ margin: '8px 0px' }}>이름</li>
+                    <li style={{ margin: '8px 0px' }}>학번</li>
+                    <li style={{ margin: '8px 0px' }}>학과</li>
+                    <li style={{ margin: '8px 0px' }}>활동인구</li>
+                    <li style={{ margin: '8px 0px' }}>정회원</li>
+                  </ol>
+                </div>
+                {member.length > 0 &&
+                  <div style={{ width: '64%' }}>
+                    <ol>
+                      <li style={{ margin: '8px 0px' }}>{member[0].last_name}</li>
+                      <li style={{ margin: '8px 0px' }}>{member[0].last_name}</li>
+                      <li style={{ margin: '8px 0px' }}>{member[0].student_number}</li>
+                      <li style={{ margin: '8px 0px' }}>{member[0].department}</li>
+                      <li style={{ margin: '8px 0px' }}>{this.check(member[0].isActivated)}</li>
+                      <li style={{ margin: '8px 0px' }}>{this.check(member[0].isRegularMember)}</li>
+                    </ol>
+                  </div>
+                }
               </div>
-            }
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '80%', border: 'solid 1px #e9eaed', margin: '12px' }}>
-            <div style={{ padding: '12px', borderBottom: 'solid 1px #e9eaed' }}>
-              내가 쓴글
             </div>
-            <div>
-              글글
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', width: '80%', height: '100%', border: 'solid 1px #d3d6db', marginLeft: '12px', backgroundColor:'#fff' }}>
+            <div style={{ padding: '12px', borderBottom: 'solid 1px #d3d6db', backgroundColor: '#f6f7f9', fontSize: '12pt', fontWeight: 'bold' }}>
+              <Icon type="edit" /> 내가 쓴글
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', padding: '12px' }}>
+              <div style={{ display: 'block'}}>
+                <div style={{ display: 'flex', marginBottom: '8px'}}>
+                  <div style={{
+                    width: '52px',
+                    height: '52px',
+                    borderRadius: '50%',
+                    position: 'relative',
+                    marginRight: '8px',
+                    overflow: 'hidden',
+                  }}
+                  >
+                    <img src="https://i.imgur.com/tAxNVWy.jpg" style={{ maxWidth: '100%', height: 'auto' }} alt="Profile-img" />
+                  </div>
+                  <div>
+                    <p style={{ fontSize: '16pt' }}>19기 심심한 바보</p>
+                    <p>2017년 10월 4일 오후 11시 22분</p>
+                  </div>
+                </div>
+              </div>
+              <div style={{ fontSize: '12pt' }}>
+                추석 연휴 아니었음 진짜 손도 안댔다...
+                <div style={{ display: 'block' }}>
+                  <img src="https://i.imgur.com/OwKtSaK.jpg" style={{ maxWidth: '100%', height: 'auto' }} alt="HK 416"/>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        {this.props.match.params.username}
       </div>
     )
   }
