327fa469e98e270f0724d80171380b03b14359fa
sGOM <pooh6195@naver.com>
2017-10-14 14:54:02 +0900

    유저페이지 만들기 #64
    <li>를 <div>로 변경하여 더 좋은 코드로 변경
    lint가 고치라고 하는 check 함수 수정

--- src/container/evee/Userpage.js
+++ src/container/evee/Userpage.js
@@ -5,23 +5,25 @@ import { Button, Icon } from 'antd'
 import { getMembers } from '../../actions'
 
 class Userpage extends Component {
+  static check(boolean) {
+    if (boolean) {
+      return <Icon type="check" />
+    }
+    return <Icon type="close" />
+  }
   componentWillMount() {
     this.props.getMembers()
-  }
-  check(boolean) {
-      if(boolean){
-          <Icon type="check" />
-      }
-      else{
-          <Icon type="close" />
-      }
   }
   render() {
     const members = this.props.members
     const username = this.props.match.params.username
     const member = members.length > 0 ? members.filter(m => m.username === username) : []
     const myMenu = {
-      color: '#365899', float: 'left', height: '44px', padding: '0px 16px', fontSize: '12pt', fontWeight: 'bold', textAlign: 'center', borderRight: '1px solid #e9eaed',
+      padding: '0px 16px',
+      fontSize: '12pt',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      borderLeft: '1px solid #e9eaed',
     }
     return (
       <div style={{
@@ -31,7 +33,7 @@ class Userpage extends Component {
         flexDirection: 'column',
       }}
       >
-        <div style={{ height: '280px', border: '1px solid #d3d6db', margin: '12px', backgroundColor:'#fff' }}>
+        <div style={{ height: '280px', border: '1px solid #d3d6db', margin: '12px', backgroundColor: '#fff' }}>
           <div style={{ height: '236px', backgroundImage: "url('https://i.imgur.com/RcuU2lm.png')", backgroundPosition: 'center 30%' }}>
             <div style={{
               border: 'solid 4px white',
@@ -48,23 +50,22 @@ class Userpage extends Component {
               <img src="https://i.imgur.com/tAxNVWy.jpg" style={{ maxWidth: '100%', height: 'auto' }} alt="Profile-img" />
             </div>
           </div>
-          <div style={{ position: 'relative', height: '44px', paddingLeft: '340px' }}>
-            <ul style={{ width: '100%', lineHeight: '2.4', fontSize: '12pt' }}>
-              <li><a className="menu" href="" style={myMenu}>내가쓴글</a></li>
-              <li><a className="menu" href="" style={myMenu}>좋아요 누른글</a></li>
-              <li><a className="menu" href="" style={myMenu}>댓글단 글</a></li>
-              <li><a className="menu" href="/members" style={myMenu}>회원들</a></li>
-              <Button type="dashed" style={{ inline: '2.4', fontSize: '12pt', marginLeft: '372px' }}>
-                <Icon type="tool" /> 프로필 수정
-              </Button>
-            </ul>
+          <div style={{ display: 'flex', paddingLeft: '340px', lineHeight: '2.4', alignItems: 'center' }}>
+            <div className="menu" style={myMenu}><a style={{ color: '#365899' }} href="">내가쓴글</a></div>
+            <div className="menu" style={myMenu}><a style={{ color: '#365899' }} href="">좋아요 누른글</a></div>
+            <div className="menu" style={myMenu}><a style={{ color: '#365899' }} href="">댓글단 글</a></div>
+            <div className="menu" style={myMenu}><a style={{ color: '#365899' }} href="/members">회원들</a></div>
+            <div style={{ borderLeft: '1px solid #e9eaed', flex: '1' }} />
+            <Button type="dashed" style={{ fontSize: '12pt', marginRight: '4px' }}>
+              <Icon type="tool" /> 프로필 수정
+            </Button>
           </div>
         </div>
         <div style={{ display: 'flex', margin: '12px', fontSize: '10pt' }}>
-          <div style={{ display: 'block', width: '20%'}}>
-            <div style={{ position: 'sticky', top: '52px', display: 'flex', flexDirection: 'column', backgroundColor:'#fff', border: 'solid 1px #d3d6db', }}>
-              <div style={{ fontSize: '12pt' ,backgroundColor: '#f6f7f9', padding: '12px', fontWeight: 'bold', borderBottom: 'solid 1px #d3d6db', }}><Icon type="user" /> 내 정보</div>
-              <div style={{ display: 'flex', padding: '8px'  }}>
+          <div style={{ display: 'block', width: '20%' }}>
+            <div style={{ position: 'sticky', top: '52px', display: 'flex', flexDirection: 'column', backgroundColor: '#fff', border: 'solid 1px #d3d6db' }}>
+              <div style={{ fontSize: '12pt', backgroundColor: '#f6f7f9', padding: '12px', fontWeight: 'bold', borderBottom: 'solid 1px #d3d6db' }}><Icon type="user" /> 내 정보</div>
+              <div style={{ display: 'flex', padding: '8px' }}>
                 <div style={{ width: '36%', textAlign: 'center', marginRight: '24px' }}>
                   <ol>
                     <li style={{ margin: '8px 0px' }}>기수</li>
@@ -82,21 +83,21 @@ class Userpage extends Component {
                       <li style={{ margin: '8px 0px' }}>{member[0].last_name}</li>
                       <li style={{ margin: '8px 0px' }}>{member[0].student_number}</li>
                       <li style={{ margin: '8px 0px' }}>{member[0].department}</li>
-                      <li style={{ margin: '8px 0px' }}>{this.check(member[0].isActivated)}</li>
-                      <li style={{ margin: '8px 0px' }}>{this.check(member[0].isRegularMember)}</li>
+                      <li style={{ margin: '8px 0px' }}>{Userpage.check(member[0].isActivated)}</li>
+                      <li style={{ margin: '8px 0px' }}>{Userpage.check(member[0].isRegularMember)}</li>
                     </ol>
                   </div>
                 }
               </div>
             </div>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '80%', height: '100%', border: 'solid 1px #d3d6db', marginLeft: '12px', backgroundColor:'#fff' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', width: '80%', height: '100%', border: 'solid 1px #d3d6db', marginLeft: '12px', backgroundColor: '#fff' }}>
             <div style={{ padding: '12px', borderBottom: 'solid 1px #d3d6db', backgroundColor: '#f6f7f9', fontSize: '12pt', fontWeight: 'bold' }}>
               <Icon type="edit" /> 내가 쓴글
             </div>
             <div style={{ display: 'flex', flexDirection: 'column', padding: '12px' }}>
-              <div style={{ display: 'block'}}>
-                <div style={{ display: 'flex', marginBottom: '8px'}}>
+              <div style={{ display: 'block' }}>
+                <div style={{ display: 'flex', marginBottom: '8px' }}>
                   <div style={{
                     width: '52px',
                     height: '52px',
@@ -117,7 +118,7 @@ class Userpage extends Component {
               <div style={{ fontSize: '12pt' }}>
                 추석 연휴 아니었음 진짜 손도 안댔다...
                 <div style={{ display: 'block' }}>
-                  <img src="https://i.imgur.com/OwKtSaK.jpg" style={{ maxWidth: '100%', height: 'auto' }} alt="HK 416"/>
+                  <img src="https://i.imgur.com/OwKtSaK.jpg" style={{ maxWidth: '100%', height: 'auto' }} alt="HK 416" />
                 </div>
               </div>
             </div>
