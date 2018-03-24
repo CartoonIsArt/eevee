2e339bf1ceca33f1b47a5172daad401f66f9bd3c
cocoapi <eksrns22tp@gmail.com>
2018-03-24 15:28:01 +0900

    흑흑 미안해요오...

--- src/container/Registration.js
+++ src/container/Registration.js
@@ -472,7 +472,8 @@ class Registration extends Component {
       })
       Modal.warning({ title: '중복되는 ID입니다.', content: '중복되는 ID입니다.' })
     })
-    if (this.responses.status < 300) {
+    if (this.responses.status < 300 &&
+      this.responses.status >= 200) {
       Modal.success({
         title: '가입 신청이 완료되었습니다!',
         content: '오늘 안으로 가입 승인이 완료될 거에요.',
