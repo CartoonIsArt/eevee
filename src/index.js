04bcbd71974b39b165d21332e504078fc2e00333
droplet92 <lhs1438@gmail.com>
2021-02-19 18:33:50 +0900

    connected-react-router에 맞춰 react-router 업데이트

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/main.scss'
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
