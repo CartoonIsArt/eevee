c6b10bb067bbfd96876145d6f3e183bde1cc7159
droplet92 <lhs1438@gmail.com>
2021-02-24 22:49:21 +0900

    굳이 import 2번 안하게 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 import { createBrowserHistory } from 'history'
-import { Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 import './App.css';
 import Nav from './container/Nav'
@@ -19,31 +19,33 @@ class App extends Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <div style={{ background: '#dfdfdf' }}>
-            { App.isNavEnabled() && (<Nav />) }
-            <div className="Container">
-              {routes.map((route, idx) =>
-                // eslint-disable-next-line
-                (
-                  <div key={idx} style={{ display: 'flex' }}>
-                    <Route
-                      path={route.path}
-                      exact={route.exact}
-                      component={route.sidebar}
-                    />
-                    <Route
-                      path={route.path}
-                      exact={route.exact}
-                      component={route.main}
-                    />
-                  </div>
-                ))}
+      <BrowserRouter>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <div style={{ background: '#dfdfdf' }}>
+              { App.isNavEnabled() && (<Nav />) }
+              <div className="Container">
+                {routes.map((route, idx) =>
+                  // eslint-disable-next-line
+                  (
+                    <div key={idx} style={{ display: 'flex' }}>
+                      <Route
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.sidebar}
+                      />
+                      <Route
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.main}
+                      />
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
-        </ConnectedRouter>
-      </Provider>
+          </ConnectedRouter>
+        </Provider>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/main.scss'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root'),
 );
