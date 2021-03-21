90b893f8a3a0fc0e41cd8250e1fd88050ca0aa78
sGOM <pooh6195@naver.com>
2021-03-21 21:17:46 +0900

    /doorlock에 들어가면 비번 바로 보이게 수정

--- src/container/Doorlock.js
+++ src/container/Doorlock.js
@@ -1,114 +1,16 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getUser } from '../actions'
-import { neogulman } from '../policy'
 
-const Button = require('antd/lib/button')
 const Icon = require('antd/lib/icon')
 
 class Doorlock extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      mode: 'warning',
-    }
-  }
-
-  verify() {
-    if (this.props.user.isRegular) {
-      this.setState({ mode: 'Regular' })
-    } else {
-      this.setState({ mode: 'Irregular' })
-    }
-  }
-
   render() {
-    if (this.state.mode === 'warning') {
-      return (
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          flex: '1',
-          width: '100%',
-          margin: '0px 8px',
-          textAlign: 'center',
-          backgroundColor: 'white',
-        }}
-        >
-          <h1 style={{
-            fontSize: '32pt',
-            borderBottom: '1px solid black',
-            padding: '20px',
-            marginBottom: '12px',
-          }}
-          >
-            동방 비밀번호
-          </h1>
-          <div style={{
-            fontSize: '24px',
-            color: 'red',
-            margin: '12px 0px',
-            padding: '12px',
-          }}
-          >
-            <Icon type="exclamation-circle" />
-            {' '}
-            동방 비밀번호를 동아리 회원 이외의 사람이나 준회원에게 알려주면 안됩니다!
-          </div>
-          <div>
-            <Button
-              style={{
-                backgroundColor: '#1976D2',
-                fontSize: '12pt',
-                color: 'white',
-                margin: '12px 0px',
-              }}
-              onClick={() => this.verify()}
-            >
-              <Icon type="double-right" />
-              {' '}
-              보러가기
-            </Button>
-          </div>
-        </div>
-      )
-    } if (this.state.mode === 'Regular') {
-      return (
-        <div style={{
-          display: 'flex',
-          flexDirection: 'colunm',
-          width: '100%',
-          marginLeft: '20px',
-          textAlign: 'center',
-          backgroundColor: 'white',
-        }}
-        >
-          <h1 style={{
-            fontSize: '32pt',
-            borderBottom: '1px solid black',
-            padding: '20px',
-            marginBottom: '12pt',
-          }}
-          >
-            동방 비밀번호
-          </h1>
-          <h1 style={{
-            margin: '12px 0px',
-            fontSize: '24pt',
-          }}
-          >
-            [동방 비번]
-          </h1>
-        </div>
-      )
-    }
-
     return (
       <div style={{
         display: 'flex',
-        flexDirection: 'colunm',
+        flexDirection: 'column',
+        flex: '1',
         width: '100%',
-        marginLeft: '20px',
+        margin: '0px 8px',
         textAlign: 'center',
         backgroundColor: 'white',
       }}
@@ -122,28 +24,21 @@ class Doorlock extends Component {
         >
           동방 비밀번호
         </h1>
-        <div style={{ margin: '12px 0px' }}>
-          <img
-            src={neogulman}
-            alt="너굴맨"
-          />
-        </div>
-        <h1
-          style={{
-            margin: '12px',
-          }}
+        <div style={{
+          fontSize: '24px',
+          color: 'red',
+          margin: '12px 0px',
+          padding: '12px',
+        }}
         >
-          &ldquo;정회원 심사부터 통과하고 오라구!!&rdquo;
-        </h1>
+          <Icon type="exclamation-circle" />
+          {' '}
+          동방 비밀번호를 동아리 회원 이외의 사람이나 준회원에게 알려주면 안됩니다!
+        </div>
+        <div>동방비번</div>
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => ({
-  user: state.user,
-})
-const mapDispatchToProps = ({
-  getUser,
-})
-export default connect(mapStateToProps, mapDispatchToProps)(Doorlock)
+export default Doorlock
