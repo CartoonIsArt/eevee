5d5b82676997689ebc07517779bcf3917e1cb67a
sGOM <pooh6195@naver.com>
2021-03-21 20:57:38 +0900

    메뉴 목록하나만 있는거 안내려가게 하고 dashboard=>enrollment 수정

--- src/container/Sider.js
+++ src/container/Sider.js
@@ -11,7 +11,7 @@ const { SubMenu } = Menu;
 class Sider extends Component {
   static getAncestorKeys(key) {
     const map = {
-      sub1: ['/dashboard'],
+      sub1: ['/enrollment'],
       sub2: ['/noties'],
     };
     return map[key] || [];
@@ -22,14 +22,6 @@ class Sider extends Component {
     this.state = {
       openKeys: [],
     }
-  }
-
-  componentWillMount() {
-    /*
-    if (this.props.user.has_logged_in === false) {
-      this.props.getUser()
-    }
-    */
   }
 
   onOpenChange(openKeys) {
@@ -80,17 +72,10 @@ class Sider extends Component {
         {
           user.isActive
           || (
-          <SubMenu
-            key="sub1"
-            title={(
-              <span>
-                <Icon type="appstore" />
-                <span>대시보드</span>
-              </span>
-            )}
-          >
-            <Menu.Item key="/dashboard">대시보드</Menu.Item>
-          </SubMenu>
+            <Menu.Item key="/enrollment">
+              <Icon type="appstore" />
+              <span>활동인구 등록</span>
+            </Menu.Item>
           )
         }
         {/* 배포후 패치해도 되는 내용
@@ -104,17 +89,10 @@ class Sider extends Component {
           <Menu.Item key="/cleaning">청소조</Menu.Item>
         </SubMenu>
         */}
-        <SubMenu
-          key="sub4"
-          title={(
-            <span>
-              <Icon type="team" />
-              <span>회원들</span>
-            </span>
-          )}
-        >
-          <Menu.Item key="/members">회원들</Menu.Item>
-        </SubMenu>
+        <Menu.Item key="/members">
+          <Icon type="team" />
+          <span>회원들</span>
+        </Menu.Item>
         <SubMenu
           key="sub5"
           title={(
@@ -125,14 +103,18 @@ class Sider extends Component {
           )}
         >
           <Menu.Item key="/law">
-            {/* <a href="https://cia.kw.ac.kr/wiki/index.php?title=%ED%9A%8C%EC%B9%99" target="_blank" rel="noopener noreferrer">회칙</a> */}
-            <a href="https://cafe.naver.com/ciapg523/62" target="_blank" rel="noopener noreferrer">회칙</a>
+            <span>회칙</span>
           </Menu.Item>
-          <Menu.Item key="/doorlock">동방 비밀번호</Menu.Item>
-          {/* 배포 후 패치
-          <Menu.Item key="/old/noties">(구) 공지</Menu.Item>
-          <Menu.Item key="/old/texts"> (구) 자유</Menu.Item>
-          */}
+          {
+            user.isRegular
+            && (
+            <Menu.Item key="/doorlock">동방 비밀번호</Menu.Item>
+            )
+            /* 배포 후 패치
+            <Menu.Item key="/old/noties">(구) 공지</Menu.Item>
+            <Menu.Item key="/old/texts"> (구) 자유</Menu.Item>
+            */
+          }
         </SubMenu>
         {
           user.isSuperUser
@@ -144,23 +126,16 @@ class Sider extends Component {
                 <Icon type="tool" />
                 <span>임원진 도구</span>
               </span>
-)}
+            )}
           >
             <Menu.Item key="/deactivate">활동인구 초기화</Menu.Item>
           </SubMenu>
           )
         }
-        <SubMenu
-          key="sub7"
-          title={(
-            <span>
-              <Icon type="frown" />
-              <span>로그아웃</span>
-            </span>
-)}
-        >
-          <Menu.Item key="/logout">로그아웃</Menu.Item>
-        </SubMenu>
+        <Menu.Item key="/logout">
+          <Icon type="frown" />
+          <span>로그아웃</span>
+        </Menu.Item>
       </Menu>
     );
   }
