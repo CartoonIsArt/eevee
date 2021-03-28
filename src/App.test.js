c18eb96cc90d6f1e7e501f71c885a7ed30a9f266
droplet92 <lhs1438@naver.com>
2021-03-28 23:39:46 +0900

    yarn test 실패하는 문제 수정

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from './store'
+import './styles/main.scss'
 
+const store = configureStore()
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>,
+    div
+  );
 });
