21505abe0f5f2b04ed1de1c694c2b6e1d6afb7c0
sGOM <pooh6195@naver.com>
2021-07-31 08:30:32 +0900

    마이페이지 수정중

--- src/container/Userpage.js
+++ src/container/Userpage.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { getMembers, logout, getUser } from '../actions'
-import { Button, Icon, Menu, Modal } from 'antd'
+import Feed from '../components/Feed'
+import { getMembers, logout, getUser, getTimeline } from '../actions'
+import { Button, Icon, Menu, Modal, Form, Input } from 'antd'
+import { isAlmostScrolled } from '../lib'
 
 class Userpage extends Component {
   constructor(props) {
@@ -11,8 +13,16 @@ class Userpage extends Component {
     this.props.getMembers()
     this.props.getUser()
     this.state = {
+      editVisible: false,
       visible: false,
+      response: '',
+      password: '',
+      page: 1,
+      doclen: 0,
+      showType: 0,
     }
+    this.mutex = true
+    this.wrapper = (e) => this.loadMore(e)
   }
 
   static check(boolean) {
@@ -23,49 +33,160 @@ class Userpage extends Component {
   }
 
   showModal() {
-    this.setState({ visible: true });
+    this.setState({ visible: true, });
   }
 
   hideModal() {
-    this.setState({ visible: false });
+    this.setState({ visible: false, });
+  }
+
+  showProfileModal(){
+    this.setState({ editVisible: true, });
+  }
+
+  hideProfileModal(){
+    console.log("Why? why?")
+    this.setState({ editVisible: false, });
   }
 
   onLogout() {
     this.props.logout();
   }
 
+  componentWillMount() {
+    this.props.getTimeline()
+    this.setState({ doclen: this.props.timeline.length })
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.wrapper)
+  }
+
+  componentWillUnmount() {
+    window.addEventListener('scroll', this.wrapper)
+  }
+
+  loadMore(e) {
+    const { page } = this.state
+    const timelinelen = this.props.timeline.length
+    e.preventDefault()
+    console.log(isAlmostScrolled())
+    if (this.mutex && isAlmostScrolled()
+      && (this.state.doclen !== timelinelen)) {
+      this.mutex = false
+      this.props.getTimeline(page + 1)
+      this.setState({
+        page: page + 1,
+        doclen: timelinelen,
+      }, () => { this.mutex = true })
+    }
+  }
+
+  clickEditProfileBtn()
+  {
+    const args = {
+      password: this.state.password,
+    }
+    axios.post('/public/login', args) // how
+      .then((r) => {
+        this.props.history.push('/settings/account')
+      })
+      .catch((e) => {
+        console.log(e)
+        this.hideProfileModal()
+        Modal.warning({ title: '비밀번호가 틀립니다.', content: '비밀번호를 확인해주세요.' })
+      })
+  }
+
+  showMine(feed)
+  {
+    const { user } = this.props
+    switch(this.state.showType)
+    {
+      case 0 :
+        if (feed.author.id === user.id)
+          return (<Feed user={user} key={feed.id} content={feed}/>)
+        break;
+
+      case 1 :
+        feed.comments.map((comment)=>{
+          console.log(comment)
+          if(comment.author.id === user.id) 
+          {
+            console.log("why so serious")
+            console.log(user)
+            console.log(feed.id)
+            console.log(feed)
+            return (<Feed user={user} key={feed.id} content={feed}/>)
+          }
+        })
+        break;
+
+      case 2:
+        feed.likedUsers.map((liker)=>{
+          if(liker.id === user.id) 
+            return (<Feed user={user} key={feed.id} content={feed}/>)
+        })
+        break;
+    }
+  }
+
   render() {
-    // 유저 페이지 넘길 때, props로 유저 아이디 넘기면 좋을 듯합니다
+    const { timeline } = this.props
     const { user } = this.props
     const { members } = this.props
     const { username } = this.props.match.params
+    const { password } = this.state
     const member = members.length > 0 ? members.find((m) => m.username === username) : []
     return (
       <div className="userpage">
         <div className="header">
-          <div className="background-image">
+          <div className="background-image" style={{backgroundImage: 'url(/images/profile_banner_default.png)'}}>
             <div className="user-profile">
-              <img className="profile-image-size" src="https://i.imgur.com/tAxNVWy.jpg" alt="Profile-img" />
+              <img className="profile-image-size" src="/images/profile_image_default.png" alt="Profile-img" />
             </div>
           </div>
           <div className="menu-bar">
             <div className="menu">
-              <Link to={`/members/${username}`}>작성한 글</Link>
+              <Link to={`/members/${username}`} onClick={() => this.setState({page: 1,doclen: 0,showType: 0,})}>작성한 글</Link>
             </div>
             <div className="menu">
-              <Link to={`/members/${username}/comments`}>작성한 댓글</Link>
+              <Link to={`/members/${username}/comments`} onClick={() => this.setState({page: 1,doclen: 0,showType: 1,})}>작성한 댓글</Link>
             </div>
             <div className="menu">
-              <Link to={`/members/${username}/likes`}>좋아요한 글</Link>
+              <Link to={`/members/${username}/likes`} onClick={() => this.setState({page: 1,doclen: 0,showType: 2,})}>좋아요한 글</Link>
             </div>
             <div className="menu last" onClick={() => this.props.history.push('/members')}>회원들</div>
             <div className="blank" />
             {member.id === user.id 
               &&(
-              <Button className="menu-btn" type="dashed" onClick={() => this.props.history.push('/settings/account')}>
+              <Button className="menu-btn" type="dashed" onClick={() => this.showProfileModal()}>
                 <Icon type="tool" />
                 {' '}
                 프로필 수정
+                <Modal 
+                  title="프로필 수정"
+                  visible={this.state.editVisible}
+                  onOk={() => this.clickEditProfileBtn()}
+                  onCancel={() => this.hideProfileModal()}
+                >
+                  <Form layout="inline">
+                    <Form.Item
+                      rules={[
+                        { required: true, message: '비밀번호를 입력해주세요!' },
+                      ]}
+                    >
+                      비밀번호 확인
+                      <Input 
+                        type="password"
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => this.setState({ password: e.target.value })}
+                      />
+                    </Form.Item>
+                  </Form>
+                </Modal>
               </Button>
             )}
           </div>
@@ -117,7 +238,22 @@ class Userpage extends Component {
               </div>
             </div>
           </div>
-          <div className="my-write-size">
+          <section style={{ padding: '0px 8px' }}>
+            {timeline.map((feed) => (
+              this.showMine(feed)
+              // (feed.author.id === user.id)
+              // ?(<Feed
+              //   user={user}
+              //   key={feed.id}
+              //   content={feed}
+              // />)
+              // :("")
+              ))
+            }
+            {console.log(timeline)}
+            {console.log(user)}
+          </section>
+          {/* <div className="my-write-size">
             <div className="my-write-title">
               <Icon type="edit" />
               {' '}
@@ -153,7 +289,7 @@ class Userpage extends Component {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     )
@@ -162,13 +298,20 @@ class Userpage extends Component {
 
 Userpage.propTypes = {
   history: PropTypes.object.isRequired,
+  getTimeline: PropTypes.func.isRequired,
+}
+
+Userpage.defaultProps = {
+  timeline: [],
 }
 
 const mapStateToProps = (state) => ({
+  timeline: state.timeline,
   members: state.members,
   user: state.user,
 })
 const mapDispatchToProps = ({
+  getTimeline,
   getMembers,
   getUser,
   logout,
