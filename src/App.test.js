4e6bf7fe145a8cf4adbff411414490cde5324af6
droplet92 <lhs1438@naver.com>
2021-08-03 00:12:37 +0900

    workflow 수정

--- src/App.test.js
+++ src/App.test.js
@@ -3,19 +3,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { ConnectedRouter } from 'connected-react-router'
-import configureStore, { history } from './store'
+import { history, store, persistor } from './store'
+import { ConfigProvider } from 'antd'
+import koKR from 'antd/lib/locale/ko_KR'
 import './styles/main.scss'
-
-const store = configureStore()
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <ConnectedRouter history={history}>
+          <ConfigProvider locale={koKR}>
+            <App />
+          </ConfigProvider>
+        </ConnectedRouter>
+      </PersistGate>
     </Provider>,
     div
   );
