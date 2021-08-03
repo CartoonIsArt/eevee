9329585844957eee9b2f9445a18cca36f8ab1672
sGOM <pooh6195@naver.com>
2021-08-03 11:32:07 +0900

    프로필수정페이지 코드 수정 (배너수정 미완성)

--- src/container/EditUserProfile.js
+++ src/container/EditUserProfile.js
@@ -11,11 +11,77 @@ import {
   Form,
   Icon,
   Modal,
-  Input
+  Input,
+  Cascader,
 } from 'antd'
 import moment from 'moment';
 
-const FormItem = Form.Item;
+const majors = [{
+  label: '전자정보공과대학',
+  value: '전자정보공과대학',
+  children: [
+    { label: '전자공학과', value: '전자공학과' },
+    { label: '전자통신공학과', value: '전자통신공학과' },
+    { label: '전기공학과', value: '전기공학과' },
+    { label: '전자융합공학과', value: '전자융합공학과' },
+    { label: '전자재료공학과', value: '전자재료공학과' },
+    { label: '로봇학부', value: '로봇학부' },
+  ],
+}, {
+  label: '소프트웨어융합대학',
+  value: '소프트웨어융합대학',
+  children: [
+    { label: '소프트웨어학부', value: '소프트웨어학부' },
+    { label: '컴퓨터정보공학부', value: '컴퓨터정보공학부' },
+    { label: '정보융합학부', value: '정보융합학부' },
+  ],
+}, {
+  label: '공과대학',
+  value: '공과대학',
+  children: [
+    { label: '건축공학과', value: '건축공학과' },
+    { label: '환경공학과', value: '환경공학과' },
+    { label: '화학공학과', value: '화학공학과' },
+    { label: '건축학과', value: '건축학과' },
+  ],
+}, {
+  label: '자연과학대학',
+  value: '자연과학대학',
+  children: [
+    { label: '수학과', value: '수학과' },
+    { label: '화학과', value: '화학과' },
+    { label: '전자바이오물리학과', value: '전자바이오물리학과' },
+    { label: '스포츠융합과학과', value: '스포츠융합과학과' },
+    { label: '정보콘텐츠학과(야)', value: '정보콘텐츠학과(야)' },
+  ],
+}, {
+  label: '인문사회과학대학',
+  value: '인문사회과학대학',
+  children: [
+    { label: '국어국문학과', value: '국어국문학과' },
+    { label: '영어산업학과', value: '영어산업학과' },
+    { label: '미디어커뮤니케이션학부', value: '미디어커뮤니케이션학부' },
+    { label: '산업심리학과', value: '산업심리학과' },
+    { label: '동북아문화산업학부', value: '동북아문화산업학부' },
+  ],
+}, {
+  label: '정책법학대학',
+  value: '정책법학대학',
+  children: [
+    { label: '행정학과', value: '행정학과' },
+    { label: '법학부', value: '법학부' },
+    { label: '국제학부', value: '국제학부' },
+    { label: '자산관리학과(야)', value: '자산관리학과(야)' },
+  ],
+}, {
+  label: '경영대학',
+  value: '경영대학',
+  children: [
+    { label: '경영학부', value: '경영학부' },
+    { label: '국제통상학부', value: '국제통상학부' },
+  ],
+},
+]
 
 function beforeUpload(file) {
   const isImage = file.type === 'image/gif'
@@ -33,6 +99,15 @@ function beforeUpload(file) {
   return isImage && isLt10M;
 }
 
+function isPermittedBirthdate(date) {
+  const max_birthdate = moment().subtract(120, 'years')
+  const min_birthdate = moment()
+
+  return date
+    && date.isBefore(max_birthdate)
+    && date.isAfter(min_birthdate)
+}
+
 class EditUserProfile extends Component {
   constructor(props) {
     super(props)
@@ -40,6 +115,7 @@ class EditUserProfile extends Component {
     console.log(this.props.user)
     this.state = {
       visible: false,
+      bVisible: false,
       id: '', //patchUser할때 넘겨줘야함
       email: this.props.user.email,
       birthdate: this.props.user.birthdate,
@@ -47,29 +123,53 @@ class EditUserProfile extends Component {
       phoneNumber: this.props.user.phoneNumber,
       favoriteComic: this.props.user.favoriteComic,
       favoriteCharacter: this.props.user.favoriteCharacter,
-      password: '',
-      profile: 'https://pbs.twimg.com/media/DLJeodaVoAAIkUU.jpg', // 기존 이미지로 설정해야됨
+      profile: this.props.user.profileImage.savedPath, // 기존 이미지로 설정해야됨
+      banner: '/images/profile_banner_default.png',// 수정필요 (user가 bannerPath를 가지게되면)
       previewVisible: false,
+      bannerPreviewVisible: false,
       fileList: [{
         uid: -1,
-        name: 'defaultImage.jpg',
+        name: 'profileImageDefault',
         status: 'done',
-        url: 'https://pbs.twimg.com/media/DLJeodaVoAAIkUU.jpg',
+        url: this.props.user.profileImage.savedPath,
+      }],
+      bannerFileList: [{
+        uid: -1,
+        name: 'bannerImageDefault',
+        status: 'done',
+        url: '/images/profile_banner_default.png', // 수정필요 (user가 bannerPath를 가지게되면)
       }],
     };
-    console.log(this.state.birthdate)
+  }
+
+  onDateChange(_, dateString) {
+    this.setState({ birthdate: dateString });
   }
 
   onChangeInput(e) {
     this.setState(e);
   }
 
-  onDateChange(date, dateString) {
-    console.log(date, dateString);
-    this.setState({ birthdate: dateString });
+  isEmpty() {
+    return !(this.state.birthdate
+            && this.state.major
+            && this.state.email
+            && this.state.phoneNumber)
   }
 
   handleOk() {
+
+    if (this.isEmpty()) {
+      return Modal.warning({ title: '다시 확인해주세요!', content: '입력하지 않은 필수 항목이 있습니다.' });
+    }
+    // https://emailregex.com/
+    if (/^(?!.*^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$).*/.test(this.state.email)) {
+      return Modal.warning({ title: '이메일을 확인해주세요!', content: '유효하지 않은 이메일 주소입니다.' })
+    }
+    if (/^(?!.*^\d{3}[-]+\d{4}[-]+\d{4}$)/.test(this.state.phoneNumber)) {
+      return Modal.warning({ title: '전화번호를 확인해주세요!', content: '유효하지 않은 전화번호입니다.' })
+    }
+
     const user = {
       id: this.props.user.id,
       email: this.state.email,
@@ -78,15 +178,14 @@ class EditUserProfile extends Component {
       phoneNumber: this.state.phoneNumber,
       favoriteComic: this.state.favoriteComic,
       favoriteCharacter: this.state.favoriteCharacter,
-      profile: this.state.profile,  //백엔드수정
-      password: this.state.password
+      profile: this.state.profile,
     }
     try{
       this.props.patchUser(user);
     }
     catch(e){
       console.log(e)
-      Modal.warning({ title: '회원정보수정에 실패했습니다.', content: '입력한 비밀번호를 확인해주세요.' })
+      Modal.warning({ title: '회원정보수정에 실패했습니다.', content: '입력정보를 확인해주세요.' })
     }
     this.setState({ visible: false });
   }
@@ -99,8 +198,20 @@ class EditUserProfile extends Component {
     this.setState({ visible: true });
   }
 
+  handleBannerCancel() {
+    this.setState({ bVisible: false });
+  }
+
+  showBannerModal(){
+    this.setState({ bVisible: true })
+  }
+
   handleCancelProfile() {
     this.setState({ previewVisible: false })
+  }
+
+  handleCancelBanner(){
+    this.setState({ bannerPreviewVisible: false })
   }
 
   handlePreview(file) {
@@ -110,18 +221,51 @@ class EditUserProfile extends Component {
     });
   }
 
+  handleBannerPreview(file) {
+    this.setState({
+      banner: file.url || file.thumbUrl,
+      bannerPreviewVisible: true,
+    });
+  }
+
   handleChange({ fileList }) {
     this.setState({ fileList })
   }
 
-  handleSubmit() {
-    this.showModal();
+  handleBannerChange({ fileList }) {
+    this.setState({ bannerFileList: fileList })
+  }
+
+  onMajorChange(value) {
+    this.setState({ major: value[1] })
+  }
+
+  onChangePhoneNumber(phoneNumber) {
+    if (/(?![0-9-]{0,13}$)/.test(phoneNumber)) {
+      return
+    }
+    if (this.isKeyBackspace
+        && (phoneNumber.length == 4 || phoneNumber.length == 9)) {
+      phoneNumber = phoneNumber.slice(0, -1)
+    }
+    if (!this.isKeyBackspace
+        && (phoneNumber.length == 4 || phoneNumber.length == 9)) {
+      phoneNumber = [
+        phoneNumber.slice(0, phoneNumber.length - 1),
+        phoneNumber.slice(phoneNumber.length - 1),
+      ].join('-')
+    }
+    this.setState({ phoneNumber })
+  }
+
+  onKeyDownBackspace(e) {
+    this.isKeyBackspace = (e.key === 'Backspace')
   }
 
   render() {
     const {
-      email, major, phoneNumber, favoriteComic, favoriteCharacter, password,
-      previewVisible, profile, fileList, birthdate
+      email, major, phoneNumber, favoriteComic, favoriteCharacter,
+      previewVisible, bannerPreviewVisible, profile, banner, fileList, bannerFileList, birthdate
     } = this.state;
     const uploadButton = (
       <div>
@@ -139,81 +283,104 @@ class EditUserProfile extends Component {
           marginTop: '8px',
           display: 'flex',
           flexDrection: 'column',
+          flexWrap: 'wrap',
         }}
         >
-          <div style={{
-            width: '400px',
-            marginTop: '52px',
-            marginLeft: '80px',
-          }}
-          >
-            <h1 style={{ marginBottom: '52px' }}> 프로필 수정 </h1>
-            <Form onSubmit={() => this.handleSubmit()}>
-              <FormItem
-                label="이메일"
-              >
-                <Input
-                  onChange={(e) => this.onChangeInput({ email: e.target.value })}
-                  placeholder="ex) example@example.com"
-                  value={email}
-                />
-              </FormItem>
-              <FormItem
-                label="생일"
-              >
-                <DatePicker
-                  defaultValue={moment(birthdate, 'YYYY-MM-DD')}
-                  onChange={(date, dateString) => this.onDateChange(date, dateString)}
-                  placeholder="날짜를 선택해주세요"
-                />
-              </FormItem>
-              <FormItem
-                label="전공"
-              >
-                <Input
-                  onChange={(e) => this.onChangeInput({ major: e.target.value })}
-                  placeholder="ex) 컴퓨터소프트웨어학과"
-                  value={major}
-                />
-              </FormItem>
-              <FormItem
-                label="핸드폰 번호"
-              >
-                <Input
-                  addonBefore="010"
-                  style={{ width: '100%' }}
-                  onChange={(e) => this.onChangeInput({ phoneNumber: e.target.value })}
-                  placeholder="ex) 1234 - 1234"
-                  value={phoneNumber}
-                />
-              </FormItem>
-              <FormItem
-                label="좋아하는 캐릭터"
-              >
-                <Input
-                  addonBefore="만화 제목"
-                  style={{ width: '100%' }}
-                  onChange={(e) => this.onChangeInput({ favoriteComic: e.target.value })}
-                  placeholder="ex) 하이큐"
-                  value={favoriteComic}
-                />
-                <Input
-                  addonBefore="캐릭터 이름"
-                  style={{ marginTop: '8px', width: '100%' }}
-                  onChange={(e) => this.onChangeInput({ favoriteCharacter: e.target.value })}
-                  placeholder="ex) 카게야마 토비오"
-                  value={favoriteCharacter}
-                />
-              </FormItem>
-              <FormItem
-                label="비밀번호 확인"
-              >
-                <Input
-                  onChange={(e) => this.onChangeInput({ password : e.target.value })}
-                  value={password}
-                />
-              </FormItem>
-            </Form>
+          <h1 style={{ margin: '12px 0px 12px 12px' }}> 프로필 수정 </h1>
+          <div className="header">
+            <div className="background-image">
+              <div className="user-banner">
+                <Upload
+                  action="//jsonplaceholder.typicode.com/posts/" // 실제로 작동할 수 있도록 작성해야 함
+                  listType="picture-card"
+                  fileList={bannerFileList}
+                  onPreview={(e) => this.handleBannerPreview(e)}
+                  onChange={(e) => this.handleBannerChange(e)}
+                  beforeUpload={(e) => beforeUpload(e)}
+                >
+                  {bannerFileList.length ? null : uploadButton}
+                </Upload>
+                <Modal
+                  visible={bannerPreviewVisible}
+                  footer={null}
+                  onCancel={() => this.handleCancelBanner()}
+                >
+                  <img
+                    alt="프로필 이미지"
+                    style={{ width: '100%' }}
+                    src={banner}
+                  />
+                </Modal>
+              </div>
+              <div className="user-profile">
+                <Upload
+                  action="//jsonplaceholder.typicode.com/posts/" // 실제로 작동할 수 있도록 작성해야 함
+                  listType="picture-card"
+                  fileList={fileList}
+                  onPreview={(e) => this.handlePreview(e)}
+                  onChange={(e) => this.handleChange(e)}
+                  beforeUpload={(e) => beforeUpload(e)}
+                >
+                  {fileList.length ? null : uploadButton}
+                </Upload>
+                <Modal
+                  visible={previewVisible}
+                  footer={null}
+                  onCancel={() => this.handleCancelProfile()}
+                >
+                  <img
+                    alt="프로필 이미지"
+                    style={{ width: '100%' }}
+                    src={profile}
+                  />
+                </Modal>
+              </div>
+            </div>
+            <div className="menu-bar"/>
+          </div>
+          <div className="input">
+            <Input
+              addonBefore="*이메일"
+              onChange={(e) => this.onChangeInput({ email: e.target.value })}
+              placeholder="ex) example@example.com"
+              value={email}
+            />
+            <DatePicker
+              defaultValue={moment(birthdate, 'YYYY-MM-DD')}
+              onChange={(_, dateString) => this.onDateChange(_, dateString)}
+              placeholder="*생일을 선택하세요"
+              disabledDate={(currentDate) => { isPermittedBirthdate(currentDate) }}
+            />
+            <Cascader
+              //defaultValue={[major]} DB를 수정하고 수정
+              style={{ width: '288px', marginBottom: '8px' }}
+              options={majors}
+              size="large"
+              onChange={(value) => this.onMajorChange(value)}
+              placeholder="*전공"
+            />
+            <Input
+              addonBefore="*전화번호"
+              style={{ width: '100%' }}
+              onChange={(e) => this.onChangePhoneNumber(e.target.value)}
+              onKeyDown={(e) => this.onKeyDownBackspace(e)}
+              placeholder="ex) 010-1234-1234"
+              value={phoneNumber}
+            />
+            <Input
+              addonBefore="만화 제목"
+              style={{ width: '100%' }}
+              onChange={(e) => this.onChangeInput({ favoriteComic: e.target.value })}
+              placeholder="ex) 하이큐"
+              value={favoriteComic}
+            />
+            <Input
+              addonBefore="캐릭터 이름"
+              style={{ marginTop: '8px', width: '100%' }}
+              onChange={(e) => this.onChangeInput({ favoriteCharacter: e.target.value })}
+              placeholder="ex) 카게야마 토비오"
+              value={favoriteCharacter}
+            />
             <div style={{
               marginTop: '80px',
               marginLeft: '400px',
@@ -229,44 +396,6 @@ class EditUserProfile extends Component {
                 <h1>정말 수정하시겠습니까?</h1>
               </Modal>
             </div>
-          </div>
-          <div style={{
-            width: '400px',
-            marginTop: '136px',
-            marginLeft: '40px',
-            display: 'flex',
-            flexDrection: 'row',
-          }}
-          >
-            <Form onSubmit={() => this.handleSubmit()}>
-              <FormItem label="프로필 사진">
-                <div style={{ marginTop: '8px' }}>
-                  <div>
-                    <Upload
-                      action="//jsonplaceholder.typicode.com/posts/" // 실제로 작동할 수 있도록 작성해야 함
-                      listType="picture-card"
-                      fileList={fileList}
-                      onPreview={(e) => this.handlePreview(e)}
-                      onChange={(e) => this.handleChange(e)}
-                      beforeUpload={(e) => beforeUpload(e)}
-                    >
-                      {fileList.length ? null : uploadButton}
-                    </Upload>
-                    <Modal
-                      visible={previewVisible}
-                      footer={null}
-                      onCancel={() => this.handleCancelProfile()}
-                    >
-                      <img
-                        alt="프로필 이미지"
-                        style={{ width: '100%' }}
-                        src={profile}
-                      />
-                    </Modal>
-                  </div>
-                </div>
-              </FormItem>
-            </Form>
           </div>
         </div>
       </LocaleProvider>
