3951a519c1a695f11f6c53b82af69b4087983951
sGOM <pooh6195@naver.com>
2021-08-04 13:05:58 +0900

    majors 변수를 common 폴더로 이동

--- src/container/EditUserProfile.js
+++ src/container/EditUserProfile.js
@@ -8,80 +8,13 @@ import {
   Upload,
   message,
   DatePicker,
-  Form,
   Icon,
   Modal,
   Input,
   Cascader,
 } from 'antd'
 import moment from 'moment';
-
-const majors = [{
-  label: '전자정보공과대학',
-  value: '전자정보공과대학',
-  children: [
-    { label: '전자공학과', value: '전자공학과' },
-    { label: '전자통신공학과', value: '전자통신공학과' },
-    { label: '전기공학과', value: '전기공학과' },
-    { label: '전자융합공학과', value: '전자융합공학과' },
-    { label: '전자재료공학과', value: '전자재료공학과' },
-    { label: '로봇학부', value: '로봇학부' },
-  ],
-}, {
-  label: '소프트웨어융합대학',
-  value: '소프트웨어융합대학',
-  children: [
-    { label: '소프트웨어학부', value: '소프트웨어학부' },
-    { label: '컴퓨터정보공학부', value: '컴퓨터정보공학부' },
-    { label: '정보융합학부', value: '정보융합학부' },
-  ],
-}, {
-  label: '공과대학',
-  value: '공과대학',
-  children: [
-    { label: '건축공학과', value: '건축공학과' },
-    { label: '환경공학과', value: '환경공학과' },
-    { label: '화학공학과', value: '화학공학과' },
-    { label: '건축학과', value: '건축학과' },
-  ],
-}, {
-  label: '자연과학대학',
-  value: '자연과학대학',
-  children: [
-    { label: '수학과', value: '수학과' },
-    { label: '화학과', value: '화학과' },
-    { label: '전자바이오물리학과', value: '전자바이오물리학과' },
-    { label: '스포츠융합과학과', value: '스포츠융합과학과' },
-    { label: '정보콘텐츠학과(야)', value: '정보콘텐츠학과(야)' },
-  ],
-}, {
-  label: '인문사회과학대학',
-  value: '인문사회과학대학',
-  children: [
-    { label: '국어국문학과', value: '국어국문학과' },
-    { label: '영어산업학과', value: '영어산업학과' },
-    { label: '미디어커뮤니케이션학부', value: '미디어커뮤니케이션학부' },
-    { label: '산업심리학과', value: '산업심리학과' },
-    { label: '동북아문화산업학부', value: '동북아문화산업학부' },
-  ],
-}, {
-  label: '정책법학대학',
-  value: '정책법학대학',
-  children: [
-    { label: '행정학과', value: '행정학과' },
-    { label: '법학부', value: '법학부' },
-    { label: '국제학부', value: '국제학부' },
-    { label: '자산관리학과(야)', value: '자산관리학과(야)' },
-  ],
-}, {
-  label: '경영대학',
-  value: '경영대학',
-  children: [
-    { label: '경영학부', value: '경영학부' },
-    { label: '국제통상학부', value: '국제통상학부' },
-  ],
-},
-]
+import majors from '../common/majors';
 
 function beforeUpload(file) {
   const isImage = file.type === 'image/gif'
