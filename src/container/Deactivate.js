bd6e75540ddb008914b7612868f39b81a20b6b8c
droplet92 <lhs1438@naver.com>
2021-08-25 03:27:06 +0900

    활동인구 초기화 페이지 css 수정

--- src/container/Deactivate.js
+++ src/container/Deactivate.js
@@ -1,119 +1,43 @@
-import { Button, Icon, Modal } from 'antd'
+import { Button, Card, message, Popconfirm, Result } from 'antd'
 import React, { Component } from 'react'
 
 
+function confirm() {
+  message.success('활동인구가 초기화되었습니다.')
+}
+
+function cancel() {
+  message.info('취소했습니다.')
+}
+
 class Deactivate extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { visible: false }
-  }
-
-  showModal() {
-    this.setState({
-      visible: true,
-      modalLevel: 0,
-    });
-  }
-
-  handleOk() {
-    if (this.state.modalLevel === 1) {
-      this.setState({ visible: false })
-    }
-    this.setState({
-      modalLevel: this.state.modalLevel + 1,
-    });
-  }
-
-  handleCancel() {
-    this.setState({
-      visible: false,
-      modalLevel: 0,
-    });
-  }
-
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          flex: '1',
-          flexDirection: 'column',
-          width: '100%',
-          marginLeft: '20px',
-          textAlign: 'center',
-          backgroundColor: 'white',
-        }}
-      >
-        <div
-          style={{
-            fontSize: '120pt',
-            backgroundColor: 'red',
-            color: 'white',
-            margin: '12px',
-          }}
-        >
-          <Icon type="exclamation-circle-o" />
-          {' '}
-          주의
-          <Icon type="exclamation-circle-o" />
-        </div>
-        <div
-          style={{
-            fontSize: '32pt',
-          }}
-        >
-          이 버튼을 잘못 누르면
-          {' '}
-          <br />
-          모두가 개고생을 하게됩니다.
-          <br />
-          신중히 생각하세요@
-        </div>
-        <div style={{ margin: '60px' }}>
-          <Button
-            type="danger"
-            onClick={() => this.showModal()}
-            size="large"
-            icon="double-right"
-            style={{
-              fontSize: '12pt',
-            }}
-          >
-            초기화하기
-          </Button>
-          { this.state.modalLevel === 0
-            ? (
-              <Modal
-                closable={false}
-                title="활동인구 초기화"
-                visible={this.state.visible}
-                footer={[
-                  <Button key="뒤로" size="large" onClick={() => this.handleCancel()}>뒤로</Button>,
-                  <Button key="초기화" type="danger" size="large" onClick={() => this.handleOk()}>
-                    초기화
-                  </Button>,
-                ]}
+      <Card className="enrollment">
+        <Result
+          status="warning"
+          title="주의!"
+          subTitle="이 버튼을 잘못 누르면 모두가 개고생을 하게 됩니다."
+          extra={
+            <Popconfirm
+              title="정말정말 초기화하시겠습니까?"
+              onConfirm={confirm}
+              onCancel={cancel}
+              okText="초기화"
+              cancelText="취소"
+              okType="danger"
+            >
+              <Button
+                type="danger"
+                size="large"
+                style={{ fontSize: '14pt' }}
               >
-                정말정말 초기화 하시겠습니까?
-              </Modal>
-            )
-            : (
-              <Modal
-                closable={false}
-                title="진짜 활동인구 초기화"
-                visible={this.state.visible}
-                footer={[
-                  <Button key="뒤로" size="large" onClick={() => this.handleCancel()}>뒤로</Button>,
-                  <Button key="정말루 초기화" type="danger" size="large" onClick={() => this.handleOk()}>
-                    정말루 초기화
-                  </Button>,
-                ]}
-              >
-                정말정말 초기화 하시겠습니까?
-              </Modal>
-            )}
-        </div>
-      </div>
+                활동인구 초기화하기
+              </Button>
+            </Popconfirm>
+          }
+        />
+      </Card>
     )
   }
 }
