db3809af4c115577028311a17bf33ca3747de279
droplet92 <lhs1438@naver.com>
2021-08-24 13:47:50 +0900

    불필요한 scss 삭제

--- src/container/Portal.js
+++ src/container/Portal.js
@@ -9,27 +9,27 @@ import ExternalLinks from '../components/ExternalLinks'
 class Portal extends Component {
   render() {
     return (
-      <Row id="Portal" type="flex" style={{ maxWidth: '1280px', marginTop: '8px' }}>
-        <Col className="item-col" xs={{ span: 0 }} xl={{ order: 1, span: 6 }}>
+      <Row type="flex" style={{ maxWidth: '1280px', marginTop: '8px' }}>
+        <Col xs={{ span: 0 }} xl={{ order: 1, span: 6 }}>
           <Affix offsetTop={56}>
             <Profile />
           </Affix>
         </Col>
-        <Col className="item-col" xs={{ order: 2, span: 24 }} xl={{ order: 2, span: 12 }}>
+        <Col xs={{ order: 2, span: 24 }} xl={{ order: 2, span: 12 }}>
           <Timeline />
         </Col>
-        <Col className="item-col" xs={{ span: 0 }} xl={{ order: 3, span: 6 }}>
+        <Col xs={{ span: 0 }} xl={{ order: 3, span: 6 }}>
           <Affix offsetTop={56}>
             <Row gutter={[0, 8]}>
               <Col xs={24}><Notifications /></Col>
-              <Col xs={0} xl={24}><ExternalLinks /></Col>
+              <Col xl={24}><ExternalLinks /></Col>
               {/* future feature
               <div style={{ height: '516px' }} > chat </div>
               */ }
             </Row>
           </Affix>
         </Col>
-        <Col className="item-col" xs={{ order: 1, span: 24 }} xl={{ span: 0 }}>
+        <Col xs={{ order: 1, span: 24 }} xl={{ span: 0 }}>
           <Notifications />
         </Col>
       </Row>
