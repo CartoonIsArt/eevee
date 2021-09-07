16edfa98fad37b5848c3f7e6bb7084bf797f5fe9
droplet92 <lhs1438@naver.com>
2021-09-08 03:17:36 +0900

    모든 youtube url에 대해서 iframe 생성 가능하게 수정

--- src/lib/remark-youtube-embed/index.js
+++ src/lib/remark-youtube-embed/index.js
@@ -1,6 +1,6 @@
 import { visit } from 'unist-util-visit';
 
-const regex = /\!\(http.*youtube.com.*?v=(.*)\)/;
+const regex = /^\!\(((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?\)$/;
 
 function remarkYoutubeEmbed() {
   return transform;
@@ -15,14 +15,14 @@ function ontext(node, index, parent) {
 
   if (!match) return;
 
-  const videoid = match[1];
+  const videoid = match[5];
 
   const iframe = {
     type: "iframe",
     data: {
       hName: "iframe",
       hProperties: {
-        videoid: videoid,
+        videoid,
         src: `https://www.youtube.com/embed/${videoid}`,
         width: '100%',
         height: 315,
