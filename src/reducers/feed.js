07983c88d55a49f18240cc9b94c5726a07cb8f51
droplet92 <lhs1438@naver.com>
2021-09-14 13:24:18 +0900

    단일피드 대댓글 바로 표시되게 구현

--- src/reducers/feed.js
+++ src/reducers/feed.js
@@ -1,17 +1,24 @@
 import { mergeObject } from '../lib'
 
 
+const appendComment = (content, comment, rootId) => {
+  if (content.id === rootId)
+    content.comments = (content.comments || []).concat([comment])
+  else
+    (content.comments || []).forEach(_comment => appendComment(_comment, comment, rootId))
+}
+
 const feed = (state = {}, action) => {
   switch (action.type) {
     case 'SETFEED':
       return action.feed
     case 'UPDATEFEED':
       return mergeObject(state, action.feed)
-    case 'UPDATECOMMENT':
+    case 'APPENDCOMMENT':
       if (state.comments) {
         state.comments = state.comments.map((comment) => {
-          if (comment.id === action.comment.id)
-            return mergeObject(comment, action.comment)
+          const root = action.comment.rootComment || action.comment.rootDocument
+          appendComment(comment, action.comment, root.id)
           return comment
         })
       }
